2c2517d6c7aebabe30ce74abb87939c9293d106a
pxperfectmike <pxperfectmike@gmail.com>
2023-05-31 15:38:39 -0700

    finalize styles before deployment optimization

--- src/taskpane/components/ModificationForm.js
+++ src/taskpane/components/ModificationForm.js
@@ -8,6 +8,8 @@ const ModificationForm = () => {
   const [response, setResponse] = useState("");
   const [commandOutput, setCommandOutput] = useState("");
   const [selectedLanguage, setSelectedLanguage] = useState(null);
+  const [processing, setProcessing] = useState(false);
+  const [idle, setIdle] = useState(true);
 
   const languageOptions = [
     { key: "chinese-simplified", text: "Chinese (Simplified)" },
@@ -23,38 +25,15 @@ const ModificationForm = () => {
     { key: "korean", text: "Korean" },
     { key: "arabic", text: "Arabic" },
     { key: "hindi", text: "Hindi" },
-    { key: "turkish", text: "Turkish" },
-    { key: "dutch", text: "Dutch" },
-    { key: "polish", text: "Polish" },
-    { key: "romanian", text: "Romanian" },
-    { key: "greek", text: "Greek" },
-    { key: "hungarian", text: "Hungarian" },
-    { key: "czech", text: "Czech" },
-    { key: "swedish", text: "Swedish" },
-    { key: "danish", text: "Danish" },
-    { key: "finnish", text: "Finnish" },
-    { key: "norwegian", text: "Norwegian" },
-    { key: "slovak", text: "Slovak" },
-    { key: "bulgarian", text: "Bulgarian" },
-    { key: "catalan", text: "Catalan" },
-    { key: "croatian", text: "Croatian" },
-    { key: "filipino", text: "Filipino" },
     { key: "hebrew", text: "Hebrew" },
-    { key: "indonesian", text: "Indonesian" },
-    { key: "lithuanian", text: "Lithuanian" },
-    { key: "malay", text: "Malay" },
-    { key: "persian", text: "Persian" },
-    { key: "thai", text: "Thai" },
-    { key: "ukrainian", text: "Ukrainian" },
     { key: "vietnamese", text: "Vietnamese" },
-    { key: "welsh", text: "Welsh" },
     // Add other languages here...
   ];
 
   const handleClick = async (action, e) => {
     e.preventDefault();
+    setProcessing(true);
     try {
-      // Get the highlighted text from Word
       const context = await Word.run(async (context) => {
         const highlight = context.document.getSelection();
         highlight.load("text");
@@ -66,27 +45,26 @@ const ModificationForm = () => {
 
       const prompt =
         action === "summarize"
-          ? `summarize this text:${context.highlight}`
-          : `translate this text:${context.highlight} to ${selectedLanguage}`;
+          ? `summarize this text ${context.highlight}`
+          : "translate"
+          ? `translate this text ${context.highlight} to ${selectedLanguage}`
+          : `elaborate this text ${context.highlight}`;
 
       const result = await axios.post("https://us-central1-cindyai.cloudfunctions.net/openai-cindy-request", {
         prompt: prompt,
       });
 
-      // Remove the initial highlight
       await Word.run(context.context, (newContext) => {
         const selection = newContext.document.getSelection();
         selection.insertText("", Word.InsertLocation.replace);
         return newContext.sync();
       });
-
+      setProcessing(false);
       setCommandOutput("Success!");
 
-      // Write out the response
       let charArray = result.data.choices[0].text.trim().split("");
 
       for (let i = 0; i < charArray.length; i++) {
-        // This will create a pause before adding the next character
         await new Promise((resolve) => setTimeout(resolve, 10));
 
         await Word.run(context.context, (newContext) => {
@@ -94,14 +72,13 @@ const ModificationForm = () => {
           body.insertText(charArray[i], Word.InsertLocation.end);
           return newContext.sync();
         });
-
-        setResponse((prev) => prev + charArray[i]);
       }
       setCommandOutput("");
     } catch (error) {
       console.error(error);
-      setResponse("An error occurred. Please try again.");
       setCommandOutput("Error!");
+    } finally {
+      setProcessing(false);
     }
   };
 
@@ -117,11 +94,10 @@ const ModificationForm = () => {
       <div
         style={{
           border: "1px solid black",
-          // padding: "10px",
           width: "200px",
           textAlign: "center",
           marginTop: "10px",
-          color: commandOutput === "Success!" ? "green" : "red",
+          color: commandOutput === "Success!" ? "green" : "Processing..." ? "black" : "red",
           borderRadius: "2px",
           margin: "2% auto",
         }}
@@ -137,21 +113,37 @@ const ModificationForm = () => {
         >
           Status:{" "}
         </strong>
-        {commandOutput}
+        {processing ? "Processing..." : idle ? "Idle" : commandOutput}
       </div>
-
-      <DefaultButton style={{ marginBottom: "2%" }} onClick={(e) => handleClick("summarize", e)}>
-        Summarize
-      </DefaultButton>
-      <Dropdown
-        style={{ marginBottom: "5%" }}
-        placeholder="Select a language"
-        options={languageOptions}
-        onChange={(_, option) => setSelectedLanguage(option.key)}
-      />
-      <DefaultButton onClick={(e) => handleClick("translate", e)} disabled={!selectedLanguage}>
-        Translate
-      </DefaultButton>
+      <div className="formatting-section" style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+        <h4>Formatting</h4>
+        <DefaultButton
+          style={{ marginBottom: "6%", width: "100%" }}
+          onClick={(e) => handleClick("summarize", e)}
+          disabled={processing}
+        >
+          Summarize
+        </DefaultButton>
+        <DefaultButton
+          style={{ marginBottom: "2%", width: "100%" }}
+          onClick={(e) => handleClick("summarize", e)}
+          disabled={processing}
+        >
+          Elaborate
+        </DefaultButton>
+      </div>
+      <div className="translate-section" style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+        <h4>Translation</h4>
+        <Dropdown
+          style={{ marginBottom: "5%" }}
+          placeholder="Select a language"
+          options={languageOptions}
+          onChange={(_, option) => setSelectedLanguage(option.key)}
+        />
+        <DefaultButton onClick={(e) => handleClick("translate", e)} disabled={!selectedLanguage || processing}>
+          Translate
+        </DefaultButton>
+      </div>
     </div>
   );
 };
